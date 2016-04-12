366104f60d09fb331571abbd99cdcc8718415fef
Gez <gezpage@gmail.com>
2016-04-12 21:37:54 +0100

    Force grep to treat tags file as text - fixes #81

--- plat/unix/update_tags.sh
+++ plat/unix/update_tags.sh
@@ -73,8 +73,8 @@ INDEX_WHOLE_PROJECT=1
 if [ -f "$TAGS_FILE" ]; then
     if [ "$UPDATED_SOURCE" != "" ]; then
         echo "Removing references to: $UPDATED_SOURCE"
-        echo "grep -v \"$UPDATED_SOURCE\" \"$TAGS_FILE\" > \"$TAGS_FILE.temp\""
-        grep -v "$UPDATED_SOURCE" "$TAGS_FILE" > "$TAGS_FILE.temp"
+        echo "grep -a -v \"$UPDATED_SOURCE\" \"$TAGS_FILE\" > \"$TAGS_FILE.temp\""
+        grep -a -v "$UPDATED_SOURCE" "$TAGS_FILE" > "$TAGS_FILE.temp"
         INDEX_WHOLE_PROJECT=0
     fi
 fi
